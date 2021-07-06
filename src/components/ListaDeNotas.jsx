39bc24122b7db22333bab56c55c10a5be61c4ead
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-07-06 15:52:17 -0300

    add css style inside a component

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
-import CardNota from './CardNota';
+import CardNota from './CardNota/CardNota';
 
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        {Array.of("Teste01", "Teste02", "Teste03").map((teste) => {
+        {Array.of("Teste01", "Teste02", "Teste03").map((teste, index) => {
           return (
-            <li>
+            <li key={index}>
               <h3>{teste}</h3>
               <CardNota />
             </li>
           );
         })}
-
-
       </ul>
     );
   }
