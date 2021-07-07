44946db0467356b673ff7a0001d3855cbce09ef2
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-07-07 13:49:03 -0300

    creating event binding on notes texts and submit button

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,22 +2,52 @@ import React, { Component } from 'react';
 import './style.css';
 
 class FormularioCadastro extends Component {
+
+  constructor() {
+    super();
+    this.titulo = "";
+    this.descricao = "";
+  }
+
+  _handleMudancaTitulo(event) {
+    event.stopPropagation();
+    this.titulo = event.target.value;
+  }
+
+  _handleMudancaDescricao(event) {
+    event.stopPropagation();
+    this.descricao = event.target.value;
+  }
+
+  _criarNota(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log(`Nota criada: ${this.titulo} | ${this.descricao}`);
+  }
+
   render() {
     return (
-      <form className="form-cadastro">
-        <input 
-          className="form-cadastro_input" 
-          type="text" 
-          placeholder="Título"
-        />
-        <textarea 
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}>
+
+        <input
           className="form-cadastro_input"
-          rows={15} 
-          placeholder="Escreva sua nota..."
+          type="text"
+          placeholder="Título"
+          onChange={this._handleMudancaTitulo.bind(this)}
         />
+
+        <textarea
+          className="form-cadastro_input"
+          rows={15}
+          placeholder="Escreva sua nota..."
+          onChange={this._handleMudancaDescricao.bind(this)}
+        />
+
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
         </button>
+
       </form>
     );
   }
