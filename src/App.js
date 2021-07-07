be0de70947ff2f0874975e219e7357a5a401ea18
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-07-07 14:42:03 -0300

    passing information between components and using state to refresh and render the current component state

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import './assets/App.css';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
   criarNota(titulo, descricao) {
-    console.log(`Criando: ${titulo} | ${descricao}`);
+    const novaNota = {titulo, descricao};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+    const novoEstado = {
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado);
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas}/>
       </section>
     );
   }
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -3,13 +3,20 @@ import CardNota from '../CardNota/';
 import './style.css';
 
 class ListaDeNotas extends Component {
+
+  // This constructor is unnecessary by the fact that
+  // no other property is defined beyond external props.
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <ul className="lista-notas">
-        {Array.of("Teste01", "Teste02", "Teste03").map((teste, index) => {
+        {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota />
+              <CardNota titulo={nota.titulo} descricao={nota.descricao}/>
             </li>
           );
         })}
