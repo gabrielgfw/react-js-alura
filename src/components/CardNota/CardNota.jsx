0c1ffed60aad715e6903c39615f35dfa4d012cb3
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-07-12 20:29:02 -0300

    loading svg file inside CardNota Component

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './style.css'; 
+import './style.css';
+import {ReactComponent as DeleteIcon} from '../../assets/img/delete-icon.svg'
 
 class CardNota extends Component {
   render() {
@@ -7,6 +8,7 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <DeleteIcon className="card-nota_icon" />
         </header>
         <p className="card-nota_texto">{this.props.descricao}</p>
       </section>
