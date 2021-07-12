013993fdab9c8dbd502a7d2f7c60c514d2c3fb47
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-07-12 20:55:31 -0300

    trash icon now deletes the clicked note

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './style.css';
-import {ReactComponent as DeleteIcon} from '../../assets/img/delete-icon.svg'
+import { ReactComponent as DeleteIcon } from '../../assets/img/delete-icon.svg'
 
 class CardNota extends Component {
+
+  apagar() {
+    const indice = this.props.indice;
+    this.props.apagarNota(indice);
+  }
+  
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <DeleteIcon className="card-nota_icon" />
+          <DeleteIcon className="card-nota_icon" onClick={this.apagar.bind(this)}/>
         </header>
         <p className="card-nota_texto">{this.props.descricao}</p>
       </section>
--- src/components/ListaDeNotas/ListaDeNotas.jsx
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -4,19 +4,18 @@ import './style.css';
 
 class ListaDeNotas extends Component {
 
-  // This constructor is unnecessary by the fact that
-  // no other property is defined beyond external props.
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <ul className="lista-notas">
         {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} descricao={nota.descricao}/>
+              <CardNota 
+                indice={index}
+                titulo={nota.titulo} 
+                descricao={nota.descricao}
+                apagarNota={this.props.apagarNota}
+              />
             </li>
           );
         })}
